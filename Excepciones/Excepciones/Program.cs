6e632eb1cfbfe5ce3538df96294e071e33a4f93e
Jon Carrasco <jcarrascobar@gmail.com>
2018-10-16 12:08:07 +0200

    ex1

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EJEMPLO 1:
+
+            try
+            {
+                int num = 5 / Convert.ToInt32(Console.ReadLine());
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Has dividido el numero entre cero");
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Has introducido una letra en vez de un numero");
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
